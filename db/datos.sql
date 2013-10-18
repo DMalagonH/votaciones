d17c5527b65cb5502cb706609d6b04867f6a1342
CamiloQuijano <camilo@altactic.com>
2013-10-18 17:25:22 -0500

    avance proyecto

---
+++ db/datos.sql
@@ -0,0 +1,25 @@
+INSERT INTO tbl_candidatos ('id', 'candidato_nombre', 'candidato_no_tarjeton') VALUES
+(NULL, 'NCand1', '1', NULL),
+(NULL, 'NCand2', '2', NULL),
+(NULL, 'NCand3', '3', NULL),
+(NULL, 'NCand4', '4', NULL),
+(NULL, 'NCand5', '5', NULL),
+(NULL, 'NCand6', '6', NULL),
+(NULL, 'NCand7', '7', NULL),
+(NULL, 'NCand8', '8', NULL),
+(NULL, 'NCand9', '9', NULL),
+(NULL, 'NCand10', '10', NULL);
+
+INSERT INTO tbl_usuarios VALUES
+(NULL, 'mail1@test.com', 'name1', 'doc1', null, null, null),
+(NULL, 'mail2@test.com', 'name2', 'doc2', null, null, null),
+(NULL, 'mail3@test.com', 'name3', 'doc3', null, null, null),
+(NULL, 'mail4@test.com', 'name4', 'doc4', null, null, null),
+(NULL, 'mail5@test.com', 'name5', 'doc5', null, null, null),
+(NULL, 'mail6@test.com', 'name6', 'doc6', null, null, null),
+(NULL, 'mail7@test.com', 'name7', 'doc7', null, null, null),
+(NULL, 'mail8@test.com', 'name8', 'doc8', null, null, null),
+(NULL, 'mail9@test.com', 'name9', 'doc9', null, null, null),
+(NULL, 'mail10@test.com', 'name10', 'doc10', null, null, null);
+
+
